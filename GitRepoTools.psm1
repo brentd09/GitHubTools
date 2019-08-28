355025dd2ada5e0190d20943a63bdc808e5f9767
Brent Denny <brent.denny@ddls.com.au>
2019-08-28 15:37:29 +1000

    added help to github copy tool

--- GitRepoTools.psm1
+++ GitRepoTools.psm1
@@ -1,11 +1,38 @@
 function Get-FilesFromRepo {
+  <#
+  .SYNOPSIS
+    This downloads files from a GitHub Repository
+  .DESCRIPTION
+    This command will copy files from a GitHub Repo and store them in a 
+    folder of your choice.
+  .EXAMPLE
+    Get-FilesFromRepo -GitHubUserName  -Repository Webproject -Destination c:\RepoCopy
+    This gets the files in the webproject repo and copies them to the the c:\RepoCopy
+    folder.
+  .PARAMETER GitHubUserName
+    This is the GitHub user site that will be used to copy the files from
+  .PARAMETER Repository
+    This is the users repository that willl be used to copy the files from, if this 
+    is not specified a menu will show asking to choose the repo from a list.
+  .PARAMETER PathInRepo
+    If the repo has sub directories you can specify the one you want 
+  .PARAMETER Destination
+    This is the path that will be used to save the files on the local machine
+  .PARAMETER FilesToRetrive
+    This a RegEx pattern that will dictate which of the files to retrieve
+  .NOTES
+    General notes
+      Written By: Brent Denny
+      Written on: 28 Aug 2019
+  #>
   [cmdletbinding()]
   Param(
-      [string]$GitHubUserName,
-      [string]$Repository = '',
-      [string]$PathInRepo = '',
-      [string]$Destination = 'D:\Allfiles',
-      [string]$FilesToRetrive = ''
+    [Parameter(Mandatory=$true)]
+    [string]$GitHubUserName,
+    [string]$Repository = '',
+    [string]$PathInRepo = '',
+    [string]$Destination = "$env:TEMP\Git\$GitHubUserName",
+    [string]$FilesToRetrive = ''
   )
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
   if ($Repository -eq '') {
